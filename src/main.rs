efb4481b82b8a8280937f88c3f9b77eccfc96102
David Steven <rikaisensei@gmail.com>
2024-05-15 22:14:30 -0500

    Create day 2

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,5 @@ mod utils;
 
 fn main() {
     solutions::day_01::run_all();
+    solutions::day_02::run_all();
 }
---
+++ src/solutions/day_02.rs
@@ -0,0 +1,12 @@
+pub fn run_a() {
+    todo!()
+}
+
+pub fn run_b() {
+    todo!()
+}
+
+pub fn run_all() {
+    run_a();
+    run_b();
+}
