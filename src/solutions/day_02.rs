6b58f1c08306d00725953e084cea97f7f2ca29e1
David Steven <rikaisensei@gmail.com>
2024-05-15 22:27:49 -0500

    update day 2 to template

--- src/solutions/day_02.rs
+++ src/solutions/day_02.rs
@@ -1,12 +1,16 @@
-pub fn run_a() {
+use crate::utils;
+
+pub fn run_a(data: impl Into<String>) -> i32 {
     todo!()
 }
 
-pub fn run_b() {
+pub fn run_b(data: impl Into<String>) -> i32 {
     todo!()
 }
 
 pub fn run_all() {
-    run_a();
-    run_b();
+    const DAY: u8 = 1;
+    let buf = utils::read_input_from_day(DAY);
+    println!("Day {:02}, exercise A answer: {}", DAY, run_a(&buf));
+    println!("Day {:02}, exercise B answer: {}", DAY, run_b(&buf));
 }
